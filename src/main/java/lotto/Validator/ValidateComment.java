0e6c18ed628d2a75f27d86b943b01e7cb509effc
lG1l <suhjw123@naver.com>
2025-11-03 13:47:40 +0900

    fix(유효성 검사): 에러 메시지 출력문구 변경

--- src/main/java/lotto/Validator/ValidateComment.java
+++ src/main/java/lotto/Validator/ValidateComment.java
@@ -6,7 +6,7 @@ public enum ValidateComment {
     PARSE_RESULT_LOTTO_ERROR("[ERROR] 콤마(,)로 구분된 정수만 입력할 수 있습니다."),
     LOTTO_COUNT_ERROR("[ERROR] 입력된 로또 리스트의 사이즈와 로또 개수가 일치하지 않습니다.\n"),
     DUPLICATE_NUMBER_ERROR("[ERROR] 중복된 숫자가 있습니다.\n"),
-    OUT_OF_RANGE_ERROR("[ERROR] 1~45 사이의 정수만 입력할 수 있습니다.\n"),
+    OUT_OF_RANGE_ERROR("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n"),
     BONUS_NUMBER_ERROR("[ERROR] 당첨 번호에 없는 정수를 입력해야 합니다.\n");
 
     private final String message;
