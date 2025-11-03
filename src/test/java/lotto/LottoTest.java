65f52851186467e7f736bbf4e86c8417d11e08d2
lG1l <suhjw123@naver.com>
2025-11-03 17:28:40 +0900

    test(유효성 검사): Lotto의 유효성 검사하는 구문 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,14 @@
 package lotto;
 
-import lotto.Domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import lotto.Domain.Lotto;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @Test
@@ -22,5 +24,37 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 45보다 큰 수가 입력되면 예외가 발생한다.")
+    @Test
+    void createLottoByOverOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+    }
+
+    @DisplayName("로또 번호에 1보다 작은 수가 입력되면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+    }
+
+    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력된 로또 리스트의 사이즈와 로또 개수가 일치하지 않습니다.\n");
+    }
+
+    @DisplayName("로또 번호와 일치하는 개수를 정확히 도출하여 index에 매칭되게 바꿔주는지 테스트")
+    @Test
+    void testMatchingNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.compare(new Lotto(List.of(1, 2, 3, 4, 5, 7)), 6))
+                .isEqualTo(3);
+    }
 }
