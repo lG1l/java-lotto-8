c1e7840820861b97e650a4b08075c43aab7a22e0
lG1l <suhjw123@naver.com>
2025-11-03 14:01:42 +0900

    feat(유효성 검사): 당첨번호에 대한 유효성 검사 구문 추가

--- src/main/java/lotto/Controller.java
+++ src/main/java/lotto/Controller.java
@@ -1,19 +1,34 @@
 package lotto;
 
+import lotto.Domain.Lotto;
+import lotto.Domain.LottoResult;
 import lotto.Domain.UserLotto;
 import lotto.IOConsole.Input;
 import lotto.IOConsole.Output;
 import lotto.Validator.Parser;
 
+import java.util.List;
+
 public class Controller {
     public void run(){
         UserLotto user = new UserLotto(inputPrice());
+        Lotto resultLotto = new Lotto(resultNumber());
     }
 
     private int inputPrice(){
         while(true){
             try{
                 return Parser.parsePurchasePrice(Input.requestLottoCount());
+            } catch (IllegalArgumentException e){
+                Output.errorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> resultNumber(){
+        while(true){
+            try{
+                return Parser.parseResultLotto(Input.requestLottoResult());
             } catch (IllegalArgumentException e){
                 Output.errorMessage(e.getMessage());
             }
--- src/main/java/lotto/Validator/Parser.java
+++ src/main/java/lotto/Validator/Parser.java
@@ -1,7 +1,13 @@
 package lotto.Validator;
 
+import lotto.IOConsole.IOComment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
     private static final int LOTTO_PRICE = 1000;
+    private static final int ARRAY_MAX_SIZE = 46;
 
     public static int parsePurchasePrice(String input) {
         try {
@@ -16,6 +22,53 @@ public class Parser {
     private static void checkDivisionBy1000(int input) {
         if (input % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ValidateComment.DIVISION_BY_1000_ERROR.getMessage());
+        }
+    }
+
+    public static List<Integer> parseResultLotto(String input) {
+        String[] splitLotto = input.split(",");
+        return checkSplitLotto(splitLotto);
+    }
+
+    private static List<Integer> checkSplitLotto(String[] input) {
+        List<Integer> resultLotto = new ArrayList<Integer>();
+
+        for (String number : input) {
+            try {
+                int lotto = Integer.parseInt(number);
+                checkOutOfRange(lotto);
+                resultLotto.add(lotto);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ValidateComment.PARSE_RESULT_LOTTO_ERROR.getMessage());
+            }
+        }
+
+        checkLottoCount(resultLotto);
+        checkDuplicate(resultLotto);
+
+        return resultLotto;
+    }
+
+    private static void checkOutOfRange(int input) {
+        if (input < 1 || input > 45) {
+            throw new IllegalArgumentException(ValidateComment.OUT_OF_RANGE_ERROR.getMessage());
+        }
+    }
+
+    private static void checkLottoCount(List<Integer> lotto) {
+        if (lotto.size() != 6) {
+            throw new IllegalArgumentException(ValidateComment.LOTTO_COUNT_ERROR.getMessage());
+        }
+    }
+
+    private static void checkDuplicate(List<Integer> lotto) {
+        boolean[] usedNumbers = new boolean[ARRAY_MAX_SIZE];
+
+        for (Integer num : lotto) {
+            if (usedNumbers[num]) {
+                throw new IllegalArgumentException(ValidateComment.DUPLICATE_NUMBER_ERROR.getMessage());
+            }
+            usedNumbers[num] = true;
         }
     }
 }
