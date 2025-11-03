44b798a21e983ed33e7f69ab848b004e2114d029
lG1l <suhjw123@naver.com>
2025-11-03 21:08:33 +0900

    test(유효성 검사): 보너스 번호 입력에 대한 유효성 검사 테스트 추가

--- src/test/java/lotto/ParserTest.java
+++ src/test/java/lotto/ParserTest.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import lotto.Domain.Lotto;
 import lotto.Validator.Parser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ParserTest {
     @DisplayName("구매 금액으로 문자를 입력할 경우 예외가 발생한다.")
     @Test
-    void createInputCharacter() {
+    void inputCharacter() {
         assertThatThrownBy(() -> Parser.parsePurchasePrice("a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 정수만 입력할 수 있습니다.\n");
@@ -18,7 +21,7 @@ public class ParserTest {
 
     @DisplayName("1000으로 나누어 떨어지지 않는 수를 입력할 경우 예외가 발생한다.")
     @Test
-    void createNotDivisibleBy1000() {
+    void notDivisibleBy1000() {
         assertThatThrownBy(() -> Parser.parsePurchasePrice("1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1000원 단위로만 구매할 수 있습니다.\n");
@@ -26,8 +29,40 @@ public class ParserTest {
 
     @DisplayName("금액을 입력할 경우 정확한 복권 개수를 도출하는 지 테스트")
     @Test
-    void findLottoAmount() {
+    void checkLottoCount() {
         assertThat(Parser.parsePurchasePrice("10000"))
                 .isEqualTo(10);
+    }
+
+    @DisplayName("정수가 아닌 문자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputBonusCharacter() {
+        assertThatThrownBy(() -> Parser.parseBonus("a",new Lotto(List.of(1,2,3,4,5,6))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 정수만 입력할 수 있습니다.\n");
+    }
+
+    @DisplayName("45보다 큰 값을 입력한 경우 예외가 발생한다.")
+    @Test
+    void inputBonusOverOfRange() {
+        assertThatThrownBy(() -> Parser.parseBonus("46",new Lotto(List.of(1,2,3,4,5,6))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+    }
+
+    @DisplayName("1보다 작은 정수를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputBonusUnderOfRange() {
+        assertThatThrownBy(() -> Parser.parseBonus("0",new Lotto(List.of(1,2,3,4,5,6))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+    }
+
+    @DisplayName("당첨 번호에 있는 숫자를 보너스 번호로 입력하면 예외가 발생한다")
+    @Test
+    void inputBonusContainsResultLotto() {
+        assertThatThrownBy(() -> Parser.parseBonus("1",new Lotto(List.of(1,2,3,4,5,6))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호에 없는 정수를 입력해야 합니다.\n");
     }
 }
