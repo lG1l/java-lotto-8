6d98e02570ab8a3c46c9e6998f5521d9df9c02ff
lG1l <suhjw123@naver.com>
2025-11-03 22:36:22 +0900

    test(유효성 검사): 당첨 번호에 관한 유효성 검사 테스트 추가

--- src/test/java/lotto/ParserTest.java
+++ src/test/java/lotto/ParserTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParserTest {
+    // 구매 금액 유효성 검사
     @DisplayName("구매 금액으로 문자를 입력할 경우 예외가 발생한다.")
     @Test
     void inputCharacter() {
@@ -34,7 +35,49 @@ public class ParserTest {
                 .isEqualTo(10);
     }
 
-    @DisplayName("정수가 아닌 문자를 입력할 경우 예외가 발생한다.")
+    // 당첨번호 유효성 검사
+    @DisplayName("당첨 번호로 정수가 아닌 문자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputResultLottoCharacter() {
+        assertThatThrownBy(() -> Parser.parseResultLotto("a,2,3,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 콤마(,)로 구분된 정수만 입력할 수 있습니다.");
+    }
+
+    @DisplayName("당첨 번호로 콤마(,)가 아닌 구분자를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputResultLottoDelimiter() {
+        assertThatThrownBy(() -> Parser.parseResultLotto("1/2/3/4/5/6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 콤마(,)로 구분된 정수만 입력할 수 있습니다.");
+    }
+
+    @DisplayName("당첨 번호로 45보다 큰 값을 입력한 경우 예외가 발생한다.")
+    @Test
+    void inputResultLottoOverOfRange() {
+        assertThatThrownBy(() -> Parser.parseResultLotto("1,2,3,4,5,46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+    }
+
+    @DisplayName("당첨 번호로 1보다 작은 정수를 입력할 경우 예외가 발생한다.")
+    @Test
+    void inputResultLottoUnderOfRange() {
+        assertThatThrownBy(() -> Parser.parseResultLotto("1,2,3,4,5,0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+    }
+
+    @DisplayName("당첨 번호의 개수로 6개를 입력하지 않을 때 예외가 발생한다.")
+    @Test
+    void inputResultLottoCountMatching() {
+        assertThatThrownBy(() -> Parser.parseResultLotto("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력된 로또 리스트의 사이즈와 로또 개수가 일치하지 않습니다.\n");
+    }
+
+    // 보너스 번호 유효성 검사
+    @DisplayName("보너스 번호로 정수가 아닌 문자를 입력할 경우 예외가 발생한다.")
     @Test
     void inputBonusCharacter() {
         assertThatThrownBy(() -> Parser.parseBonus("a", new Lotto(List.of(1, 2, 3, 4, 5, 6))))
@@ -42,7 +85,7 @@ public class ParserTest {
                 .hasMessage("[ERROR] 정수만 입력할 수 있습니다.\n");
     }
 
-    @DisplayName("45보다 큰 값을 입력한 경우 예외가 발생한다.")
+    @DisplayName("보너스 번호로 45보다 큰 값을 입력한 경우 예외가 발생한다.")
     @Test
     void inputBonusOverOfRange() {
         assertThatThrownBy(() -> Parser.parseBonus("46", new Lotto(List.of(1, 2, 3, 4, 5, 6))))
@@ -50,7 +93,7 @@ public class ParserTest {
                 .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
     }
 
-    @DisplayName("1보다 작은 정수를 입력할 경우 예외가 발생한다.")
+    @DisplayName("보너스 번호로 1보다 작은 정수를 입력할 경우 예외가 발생한다.")
     @Test
     void inputBonusUnderOfRange() {
         assertThatThrownBy(() -> Parser.parseBonus("0", new Lotto(List.of(1, 2, 3, 4, 5, 6))))
